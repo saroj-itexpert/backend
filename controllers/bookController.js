eea1d4c8f8ead48ba27374c7300c58e2cba985d3
Saroj Maharjan <maharjan.saroj93@gmail.com>
2025-05-22 14:34:48 +0545

    message from fetch

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -3,98 +3,93 @@ const db = require("../database/connection"); // adjust path as per your project
 const books = db.books;
 
 exports.fetchBooks = async (req, res) => {
-    try {
-        //logic to fetch books from database
-        const datas = await books.findAll();
-        res.json({
-            "message": "Book fetched successfully!",
-            "datas": datas
-        });
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    //logic to fetch books from database
+    const datas = await books.findAll();
+    res.json({
+      message: "Book fetched successfully!",
+      datas: datas,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 exports.addBook = async (req, res) => {
-    try {
-        //logic to save books in database
-        //console.log(req.body.bookName);
-        const { bookKoName, bookPrice, bookAuthor } = req.body
-        console.log(bookKoName);
-        console.log(bookPrice);
-        console.log(bookAuthor);
+  try {
+    //logic to save books in database
+    //console.log(req.body.bookName);
+    const { bookKoName, bookPrice, bookAuthor } = req.body;
+    console.log(bookKoName);
+    console.log(bookPrice);
+    console.log(bookAuthor);
 
-        await books.create({
-            bookKoName,
-            bookPrice,
-            bookAuthor,
-        })
-        res.json({ "message": "Book saved successfully!" });
-    } catch (error) {
-        console.log(error);
-
-    }
-}
+    await books.create({
+      bookKoName,
+      bookPrice,
+      bookAuthor,
+    });
+    res.json({ message: "Book saved successfully!" });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 exports.deleteBook = async (req, res) => {
-    //logic to delete books from database
-    try {
-        const id = req.params.id        // const {id} = req.params (yesari pani destructure bata garna milcha)
+  //logic to delete books from database
+  try {
+    const id = req.params.id; // const {id} = req.params (yesari pani destructure bata garna milcha)
 
-        //id paaisakepachi, tyo id ko bok chai books table bata udaidine
-        await books.destroy({
-            where: {
-                id,
-            }
-        })  // delete from books where id = id;
+    //id paaisakepachi, tyo id ko bok chai books table bata udaidine
+    await books.destroy({
+      where: {
+        id,
+      },
+    }); // delete from books where id = id;
 
-        res.json({ "message": `Book deleted successfully!` });
-    } catch (error) {
-        console.log(error);
-
-    }
-
-}
+    res.json({ message: `Book deleted successfully!` });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 exports.editBook = async function (req, res) {
-    //kun id ko edit garne tesko id linu paryo 
-    try {
-        const id = req.params.id;
-        const { bookKoName, bookPrice, bookAuthor } = req.body
+  //kun id ko edit garne tesko id linu paryo
+  try {
+    const id = req.params.id;
+    const { bookKoName, bookPrice, bookAuthor } = req.body;
 
-        await books.update({ bookKoName, bookPrice, bookAuthor }, {
-            where: {
-                id: id
-            }
-        })
-        res.json({ "message": "Edited successfully!" })
-    } catch (error) {
-        console.log(error);
-
-    }
-
-}
+    await books.update(
+      { bookKoName, bookPrice, bookAuthor },
+      {
+        where: {
+          id: id,
+        },
+      }
+    );
+    res.json({ message: "Edited successfully!" });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 exports.singleFetchBook = async function (req, res) {
-    //first capture what id is he sending
-    try {
-        const id = req.params.id;
-        const datas = await books.findByPk(id);   //always returns object
-        // books.findAll({  it returns values in Array
-        //     where: {
-        //         id: id
-        //     }
-        // })
-        res.json({
-            message: "Single book is fetched successfully",
-            datas: datas
-        })
-    } catch (error) {
-        console.log(error);
-
-    }
-
-
-}
+  //first capture what id is he sending
+  try {
+    const id = req.params.id;
+    const datas = await books.findByPk(id); //always returns object
+    // books.findAll({  it returns values in Array
+    //     where: {
+    //         id: id
+    //     }
+    // })
+    res.json({
+      message: "Single book is fetched successfully",
+      datas: datas,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 // module.exports = {fetchBooks, addBook, deleteBook, editBook};  you can export methods like this too.
